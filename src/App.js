15162c2f5cffd69260435d33ff52cab3dd3032ad
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2024-07-24 18:20:11 +0530

    added alt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import Home from './components/Home';
-import Navbar from './components/Navbar';
+
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 function App() {
   return (
     <>
-    <Navbar/>
+
     <Home/>
     <Skills/>
     <Projects/>
